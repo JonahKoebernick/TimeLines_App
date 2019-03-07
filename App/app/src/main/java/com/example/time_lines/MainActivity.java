b0fe5f6ebbf6e88a6c4852ba6151f5be3d195132
jonahkoebernick <jojo52jojo52@gmail.com>
2019-03-07 13:04:55 -0800

    Nothing

--- App/app/src/main/java/com/example/time_lines/MainActivity.java
+++ App/app/src/main/java/com/example/time_lines/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         // Get access to the custom title view
         TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        
     }
 }
