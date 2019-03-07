e0f07b6b514e877b9ea94cd630f6f505e0540342
jonahkoebernick <jojo52jojo52@gmail.com>
2019-03-07 10:41:16 -0800

    Added Basic toolbar

--- App/app/src/main/java/com/example/time_lines/MainActivity.java
+++ App/app/src/main/java/com/example/time_lines/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.time_lines;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        // Get access to the custom title view
+        TextView mTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
     }
 }
