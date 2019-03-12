de3c213e2486eb372137fa8f07accb7f5a7c03f9
Jack Patterson <jackrcpatterson@gmail.com>
2019-03-11 17:32:33 -0700

    hope this doesn't fuck it up

---
+++ App/app/src/main/java/com/example/time_lines/UVicPrompt.java
@@ -0,0 +1,13 @@
+package com.example.time_lines;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UVicPrompt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uvic_prompt);
+    }
+}
